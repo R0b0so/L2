dc1668b6854df7c05b00f40d8624350ac2a81331
R0b0so <ethansagar222@gmail.com>
2024-09-25 20:05:16 -0700

    helooo

---
+++ HOMEWORK.py
@@ -0,0 +1,6 @@
+name = input("Enter your name ")
+siblingbirthday = input("Enter sibling birthday (Leave blank if none) ")
+mombirthday = input("Enter mom birthday (Leave blank if none) ")
+dadbirthday = input("Enter dad birthday (Leave blank if none) ")
+Birthday = input("Enter birthday ")
+print("Hello", name,  "I will remember your birthday which is ", Birthday, ", I will also remember your family's birthdays like your dads:", dadbirthday, "also your moms", mombirthday, "and your sibling", siblingbirthday)
